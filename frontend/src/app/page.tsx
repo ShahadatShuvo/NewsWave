7aa665a1986b986871b3b71592a76167e28a6669
mdmahin.pro <mdmahin.pro@gmail.com>
2023-07-27 15:46:23 +0600

    frontend structured

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,17 @@
 import Footer from "@/componants/homepage/footer";
 import MainCategories from "@/componants/homepage/main-categories";
 import Navbar from "@/componants/homepage/navbar";
+import SingleNews from "@/componants/single-news";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <MainCategories />
+      <div className="my-8">
+        {" "}
+        <MainCategories />
+      </div>
+      <SingleNews />
       <Footer />
     </div>
   );
