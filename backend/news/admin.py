e091e6ef6d247b7a7f1d9abc8a55abdf867065b8
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-08-02 09:12:23 +0600

    working on database

--- backend/news/admin.py
+++ backend/news/admin.py
@@ -5,8 +5,8 @@ from .models import Post, Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_categories',
-                    'description', 'created_at', 'updated_at', 'img_preview')
+    list_display = ('title', 'id', 'author', 'display_categories',
+                    'short_description', 'created_at', 'updated_at', 'img_preview')
 
     def display_categories(self, obj):
         return ", ".join([str(category) for category in obj.category.all()])
--- backend/news/models/post.py
+++ backend/news/models/post.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="posts")
     category = models.ManyToManyField(Category, blank=True)
-    description = models.TextField(null=True, blank=True)
+    short_description = models.TextField(null=True, blank=True)
     content = RichTextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
